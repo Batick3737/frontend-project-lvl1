305c122562ca076dc3c88db4310a6d2950c0d7dd
root <root@DESKTOP-RNTLF0P.localdomain>
2020-10-04 14:26:54 +0300

    Added logic to all games

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import readlineSync from 'readline-sync';
+
+console.log("Welcome to the Brain Games!");
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const game = (questionGame, correctAnswer) => {
+  console.log(`Question: ${questionGame}`);
+  const answer = readlineSync.question('Your answer: ');
+  if (answer === correctAnswer) {
+    console.log("Correct!");
+  if (round === 2) {
+    console.log(`Congratulations, ${name}!`);
+    return;
+  } else {
+    games(round + 1);
+  }
+  } else {
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".
+    Let's try again, ${name}!`);
+  }
+};
