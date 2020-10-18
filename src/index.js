01b736edcda823f9fdf93cf9e5f6fd0e42d5e124
root <root@DESKTOP-RNTLF0P.localdomain>
2020-10-18 12:40:42 +0300

    Changed the code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import readlineSync from 'readline-sync';
-export const engineGame = () => {
-console.log("Welcome to the Brain Games!");
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const games = () => {
-  console.log(`Question: ${questionGame}`);
-  const answer = readlineSync.question('Your answer: ');
-  if (answer === correctAnswer) {
-    console.log("Correct!");
-  if (round === 2) {
-    console.log(`Congratulations, ${name}!`);
-    return;
-  } else {
-    games(round + 1);
+
+export const gameEngine = (gameDescription, generateQuestion, formulateQuestion, getCorrectAnswer) => {
+  console.log("Welcome to the Brain Games!");
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(gameDescription());
+  const startRounds = (round = 0) => {
+    const question = generateQuestion();
+    const correctAnswer = getCorrectAnswer(question);
+    console.log(`Question: ${formulateQuestion(question)}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log("Correct!");
+      if (round === 2) {
+        console.log(`Congratulations, ${name}!`);
+        return;
+      } else {
+        startRounds(round + 1);
+      }
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".
+      Let's try again, ${name}!`);
+    }   
   }
-  } else {
-    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".
-    Let's try again, ${name}!`);
-  }
- }
+  startRounds();
 }
